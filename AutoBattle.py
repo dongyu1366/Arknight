09af8faefd012afd3f8d8242f6035e1d80f8722a
dongyu1366 <dongyu1366@gmail.com>
2020-10-31 16:45:40 +0800

    Combined and sorted codes, using thread and recursive

--- AutoBattle.py
+++ AutoBattle.py
@@ -1,15 +1,9 @@
 import cv2
 import pyautogui
+import threading
+from queue import Queue
 from pynput.keyboard import Key, Listener
 from time import localtime, sleep, strftime, time
-
-
-def on_release(key):
-    global running
-    if key == Key.esc:
-        running = False
-        print('程式中止中，請稍後！')
-        return False
 
 
 class AutoBattle:
@@ -25,8 +19,8 @@ class AutoBattle:
 
     @classmethod
     def initialize(cls):
-        # Expected total costing time
-        predict_time_cost = (cls.time_per_battle + 30) * cls.battles
+        # Expected costing time
+        predict_time_cost = cls.time_per_battle * cls.battles
         cost_min = predict_time_cost // 60
         cost_second = predict_time_cost % 60
         print(f'預計總花費時間為 {cost_min}分{cost_second}秒')
@@ -39,61 +33,91 @@ class AutoBattle:
         sleep(5)
 
     @classmethod
-    def battle(cls):
+    def search_target(cls, q, image, stage, waiting_time, complete):
+        mouse_position = pyautogui.locateCenterOnScreen(image, confidence=0.8)
+        if mouse_position:
+            q.put(mouse_position)
+            q.put(stage)
+            q.put(waiting_time)
+            q.put(complete)
 
-        for i in range(cls.battles):
-            if not running:
-                break
+    @classmethod
+    def battle(cls, battle_round):
+        if running:
+            q = Queue()
+            threads = [threading.Thread(target=cls.search_target, args=(q, cls.img_stage_one, 1, 3, False,)),
+                       threading.Thread(target=cls.search_target,
+                                        args=(q, cls.img_stage_two, 2, (cls.time_per_battle - 20), False,)),
+                       threading.Thread(target=cls.search_target, args=(q, cls.img_complete, 3, 5, True,))]
 
-            start_time = time()
-            start_time_str = strftime('%H:%M:%S', localtime(start_time))
-            counts = 1
-            print(f'----------Battle({i + 1}) starts at {start_time_str}----------')
-            while running:
-                stage_one = pyautogui.locateCenterOnScreen(cls.img_stage_one, confidence=0.8)
-                stage_two = pyautogui.locateCenterOnScreen(cls.img_stage_two, confidence=0.8)
-                stage_complete = pyautogui.locateCenterOnScreen(cls.img_complete, confidence=0.8)
-                if stage_one:
-                    pyautogui.click(stage_one, clicks=2, interval=0.25)
-                    print(f'Completed Stage One')
-                    sleep(2)
-                elif stage_two:
-                    pyautogui.click(stage_two, clicks=2, interval=0.25)
-                    print(f'Completed Stage Two')
-                    sleep(cls.time_per_battle)
-                elif stage_complete:
-                    sleep(5)
-                    pyautogui.click(stage_complete, clicks=2, interval=0.25)
-                    sleep(5)
-                    cls.finish_battles += 1
-                    end_time = time()
-                    cost_time = round(end_time - start_time, 2)
-                    print(f'Completed Battle({i + 1}) in {cost_time}s! \n')
-                    break
-                else:
-                    print(f'Wait a moment({counts})')
-                    counts += 1
-                    sleep(5)
+            for thread in threads:
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+
+            # Executed click
+            if not q.empty():
+                data = []
+                for _ in range(4):
+                    data.append(q.get())
+
+                sleep(2)
+                pyautogui.click(data[0], clicks=2, interval=0.25)
+                print(f'Stage {data[1]} completed!')
+                sleep(data[2])
+
+                # Finish the battle round or not
+                if not data[3]:
+                    cls.battle(battle_round)
+
+            else:
+                print('Battle processing......')
+                sleep(5)
+                cls.battle(battle_round)
 
     @classmethod
     def run(cls):
 
         cls.initialize()
+        start_time = time()
 
-        start_time = int(time())
-        cls.battle()
+        # Start battles
+        for i in range(cls.battles):
+            if not running:
+                break
+
+            # The battle starting message
+            battle_round = i + 1
+            battle_start_time = time()
+            battle_start_time_str = strftime('%H:%M:%S', localtime(battle_start_time))
+            print(f'----------Battle({battle_round}) starts at {battle_start_time_str}----------')
+
+            cls.battle(battle_round)
+
+            # The battle ending message
+            cls.finish_battles += 1
+            battle_end_time = time()
+            battle_spend_time = round(battle_end_time - battle_start_time, 2)
+            print(f'Completed Battle({battle_round}) in {battle_spend_time}s! \n')
 
         # Calculate total cost time
-        complete_time = int(time())
-        final_time_cost = complete_time - start_time
+        complete_time = time()
+        final_time_cost = int(complete_time - start_time)
         final_cost_min = final_time_cost // 60
         final_cost_second = final_time_cost % 60
         print(f'完成{cls.finish_battles}場戰鬥，總花費時間為 {final_cost_min}分{final_cost_second}秒')
 
 
-if __name__ == '__main__':
+def on_release(key):
+    global running
+    if key == Key.esc:
+        running = False
+        print('程式中止中，請稍後！')
+        return False
 
-    # Set up and load needed images
+
+if __name__ == '__main__':
     running = True
 
     lis = Listener(on_release=on_release)
